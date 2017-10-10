68d34186d95c13656d44c49cb3cb3bd899cc0966
Issa Rice <riceissa@gmail.com>
2017-10-10 10:48:57 -0700

    Add more metrics

--- proc_pwt90.py
+++ proc_pwt90.py
@@ -63,6 +63,111 @@ cols = {
             "units": "US=1",
             "metric": "TFP level at current PPPs",
         },
+        "cwtfp": {
+            "units": "Unitless",
+            "metric": "Welfare-relevant TFP levels at current PPPs (USA=1)",
+        },
+        "rgdpna": {
+            "units": "2011 international dollar",
+            "metric": "Real GDP at constant 2011 national prices",
+            "factor": 1e6,
+        },
+        "rconna": {
+            "units": "2011 international dollar",
+            "metric": "Real consumption at constant 2011 national prices",
+            "factor": 1e6,
+        },
+        "rdana": {
+            "units": "2011 international dollar",
+            "metric": "Real domestic absorption at constant 2011 national prices",
+            "factor": 1e6,
+        },
+        "rkna": {
+            "units": "2011 international dollar",
+            "metric": "Capital stock at constant 2011 national prices",
+            "factor": 1e6,
+        },
+        "rtfpna": {
+            "units": "Unitless",
+            "metric": "TFP at constant national prices (2011=1)",
+        },
+        "rwtfpna": {
+            "units": "Unitless",
+            "metric": "Welfare-relevant TFP at constant national prices (2011=1)",
+        },
+        "labsh": {
+            "units": "Unitless",
+            "metric": "Share of labour compensation in GDP at current national prices",
+        },
+        "delta": {
+            "units": "Unitless",
+            "metric": "Average depreciation rate of the capital stock",
+        },
+        "xr": {
+            "units": "Unitless",
+            "metric": "Exchange rate, national currency/USD (market+estimated)",
+        },
+        "pl_con": {
+            "units": "Unitless",
+            "metric": "Price level of CCON (PPP/XR), price level of USA GDPo in 2011=1",
+        },
+        "pl_da": {
+            "units": "Unitless",
+            "metric": "Price level of CDA (PPP/XR), price level of USA GDPo in 2011=1",
+        },
+        "pl_gdpo": {
+            "units": "Unitless",
+            "metric": "Price level of CGDPo (PPP/XR), price level of USA GDPo in 2011=1",
+        },
+        "csh_c": {
+            "units": "Unitless",
+            "metric": "Share of household consumption at current PPPs",
+        },
+        "csh_i": {
+            "units": "Unitless",
+            "metric": "Share of gross capital formation at current PPPs",
+        },
+        "csh_g": {
+            "units": "Unitless",
+            "metric": "Share of government consumption at current PPPs",
+            "factor": 1e6,
+        },
+        "csh_x": {
+            "units": "Unitless",
+            "metric": "Share of merchandise exports at current PPPs",
+        },
+        "csh_m": {
+            "units": "Unitless",
+            "metric": "Share of merchandise imports at current PPPs",
+        },
+        "csh_r": {
+            "units": "Unitless",
+            "metric": "Share of residual trade and GDP statistical discrepancy at current PPPs",
+        },
+        "pl_c": {
+            "units": "Unitless",
+            "metric": "Price level of household consumption,  price level of USA GDPo in 2011=1",
+        },
+        "pl_i": {
+            "units": "Unitless",
+            "metric": "Price level of capital formation, price level of USA GDPo in 2011=1",
+        },
+        "pl_g": {
+            "units": "Unitless",
+            "metric": "Price level of government consumption,  price level of USA GDPo in 2011=1",
+        },
+        "pl_x": {
+            "units": "Unitless",
+            "metric": "Price level of exports, price level of USA GDPo in 2011=1",
+        },
+        "pl_m": {
+            "units": "Unitless",
+            "metric": "Price level of imports, price level of USA GDPo in 2011=1",
+        },
+        "pl_k": {
+            "units": "Unitless",
+            "metric": "Price level of the capital stock, price level of USA in 2011=1",
+        },
 }
 
 
