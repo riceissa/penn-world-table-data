1ebe521f019c409fbc57f9eeb83df6d0561bae36
Issa Rice <riceissa@gmail.com>
2017-10-13 11:38:23 -0700

    add plotting example

---
+++ graphing.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import mysql.connector
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+# A simple plot created by directly querying the db
+
+cnx = mysql.connector.connect(user='issa', database='devecondata')
+
+df = pd.read_sql("select * from data where database_url = 'http://www.rug.nl/ggdc/docs/pwt56_forweb.xls'", con=cnx)
+
+jpn = df[(df['metric'] == 'Component shares of CGDP: Consumption') & (df['region'] == 'JAPAN')]
+
+plt.plot(jpn.year, jpn.value)
+
+plt.show()
